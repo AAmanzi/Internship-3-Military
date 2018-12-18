b26c837e233fce77be42f4d3bfe5aca501f59200
AAmanzi <alnick.amanzi@gmail.com>
2018-12-19 00:30:25 +0100

    Created classes, interfaces and first implementation of Swim and Move methods

---
+++ military/Warship.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace military
+{
+    sealed class Warship : Vehicle, ISwimmable
+    {
+        public Warship(int weightInKg, int averageSpeedInKmPh)
+        {
+            VehicleID = Guid.NewGuid();
+            WeightInKg = weightInKg;
+            AverageSpeedInKmPh = averageSpeedInKmPh;
+            FuelConsumptionInLitres = (int)FuelConsuption.Warship;
+            CapacityInNumberOfPeople = (int)Capacity.Warship;
+        }
+
+        public int Swim(int distanceInKm)
+        {
+            var newDistance = distanceInKm;
+            var timeToTravelInMinutes = (distanceInKm / AverageSpeedInKmPh) / 60;
+
+            for (int i = 0; i < timeToTravelInMinutes / 10; i++)
+            {
+                if (new Random().Next(0, 100) < 50)
+                    newDistance += 3;
+            }
+
+            return newDistance;
+        }
+    }
+
+
+}
