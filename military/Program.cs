94bbb26215e5f693af5e66adc9b24cf1a5b1c409
AAmanzi <alnick.amanzi@gmail.com>
2018-12-22 19:25:25 +0100

    Disabled user inputs for too long amphibian distances

--- military/Program.cs
+++ military/Program.cs
@@ -24,33 +24,47 @@ namespace military
                 while (!int.TryParse(Console.ReadLine(), out peopleToTransport))
                     Console.WriteLine("That is not a valid input, please try again:");
 
-                Console.WriteLine("Enter the distance the tank " +
+                Console.WriteLine("Enter the distance the TANK " +
                     "needs to overcome in kilometers:");
 
                 var distanceTank = 0;
                 while (!int.TryParse(Console.ReadLine(), out distanceTank))
                     Console.WriteLine("That is not a valid input, please try again:");
 
-                Console.WriteLine("Enter the distance the warship " +
+                Console.WriteLine("Enter the distance the WARSHIP " +
                     "needs to overcome in kilometers:");
 
                 var distanceWarship = 0;
                 while (!int.TryParse(Console.ReadLine(), out distanceWarship))
                     Console.WriteLine("That is not a valid input, please try again:");
 
-                Console.WriteLine("Enter the distance the amphibian " +
-                    "needs to overcome by sea in kilometers:");
-
-                var distanceAmphibianBySea = 0;
-                while (!int.TryParse(Console.ReadLine(), out distanceAmphibianBySea))
-                    Console.WriteLine("That is not a valid input, please try again:");
-
-                Console.WriteLine("Enter the distance the amphibian " +
-                    "needs to overcome by land in kilometers:");
-
+                
                 var distanceAmphibianByLand = 0;
-                while (!int.TryParse(Console.ReadLine(), out distanceAmphibianByLand))
-                    Console.WriteLine("That is not a valid input, please try again:");
+                var distanceAmphibianBySea = 0;
+
+                while (true)
+                {
+                    Console.WriteLine("Enter the distance the AMPHIBIAN " +
+                    "needs to overcome by SEA in kilometers:");
+
+                    while (!int.TryParse(Console.ReadLine(), out distanceAmphibianBySea))
+                        Console.WriteLine("That is not a valid input, please try again:");
+
+                    Console.WriteLine("Enter the distance the AMPHIBIAN " +
+                        "needs to overcome by LAND in kilometers:");
+
+                    while (!int.TryParse(Console.ReadLine(), out distanceAmphibianByLand))
+                        Console.WriteLine("That is not a valid input, please try again:");
+
+                    if (distanceAmphibianBySea + distanceAmphibianByLand > distanceTank ||
+                        distanceAmphibianBySea + distanceAmphibianByLand > distanceWarship)
+                    {
+                        Console.WriteLine("\n\nThe distance the amphibian has to travel MUST be " +
+                            "shorter than the ones the tank and warship have to cross!\n\n");
+                    }
+                    else
+                        break;
+                }
 
                 var totalDistanceTank = myTank.Move(distanceTank);
                 var totalDistanceWarship = myWarship.Swim(distanceWarship);
@@ -64,18 +78,8 @@ namespace military
                 var totalFuelConsumptionAmphibian = myWarship.FuelConsumptionTotal
                     (totalDistanceAmphibian, peopleToTransport);
 
-                var leastFuelConsumption = totalFuelConsumptionTank;
-                var bestTransport = "Tank";
-                if (leastFuelConsumption > totalFuelConsumptionWarship)
-                {
-                    leastFuelConsumption = totalFuelConsumptionWarship;
-                    bestTransport = "Warship";
-                }
-                if (leastFuelConsumption > totalFuelConsumptionAmphibian)
-                {
-                    leastFuelConsumption = totalFuelConsumptionAmphibian;
-                    bestTransport = "Amphibian";
-                }
+                var bestTransport = LeastFuelSpent(totalFuelConsumptionTank, 
+                    totalFuelConsumptionWarship, totalFuelConsumptionAmphibian);
 
                 Console.WriteLine($"\nThe best option for transport is: {bestTransport}");
 
@@ -92,15 +96,31 @@ namespace military
                         break;
                 }
 
-                Console.WriteLine("\nAmphibian Fuel: " + totalFuelConsumptionAmphibian);
-                Console.WriteLine("Tank Fuel: " + totalFuelConsumptionTank);
-                Console.WriteLine("Warship Fuel: " + totalFuelConsumptionWarship + "\n");
-
-                Console.WriteLine("Would you like to make another shipment?");
-                Console.WriteLine("Press Y for Yes");
-                Console.WriteLine("Press anything else for No");
+                Console.WriteLine("\nWould you like to make another shipment?");
+                Console.WriteLine(" ___________________________");
+                Console.WriteLine("|                           |");
+                Console.WriteLine("|Press Y            for Yes |");
+                Console.WriteLine("|Press anything else for No |");
+                Console.WriteLine("|___________________________|");
             }
             while (Console.ReadKey().Key == ConsoleKey.Y);
+        }
+
+        static string LeastFuelSpent(int fuelTankSpent, int fuelWarshipSpent, int fuelAmphibianSpent)
+        {
+            var leastFuelConsumption = fuelTankSpent;
+            var bestTransport = "Tank";
+            if (leastFuelConsumption > fuelWarshipSpent)
+            {
+                leastFuelConsumption = fuelWarshipSpent;
+                bestTransport = "Warship";
+            }
+            if (leastFuelConsumption > fuelAmphibianSpent)
+            {
+                leastFuelConsumption = fuelAmphibianSpent;
+                bestTransport = "Amphibian";
+            }
+            return bestTransport;
         }
     }
 }
